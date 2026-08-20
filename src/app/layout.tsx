372d58071bb2a54c2b99a14b3e2af63b8070a66b
Josh <josh.duncanson@gmail.com>
2026-08-20 18:15:06 -0400

    Add Vercel Web Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import { Michroma, Montserrat } from "next/font/google";
 import "./globals.css";
 import Header from "../components/Header";
@@ -54,6 +55,7 @@ export default function RootLayout({
           {children}
         </main>
         <Footer />
+        <Analytics />
       </body>
     </html>
   );
